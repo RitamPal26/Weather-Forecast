ff9f930a751b59dfd87ab7efa2f5492dccaece5b
Ritam Pal <ritamjunior26@gmail.com>
2024-05-02 18:44:50 +0530

    comit

--- src/api.js
+++ src/api.js
@@ -1,13 +1,15 @@
 
 export const geoApiOptions = {
-    method: 'GET',
-    headers: {
-        'X-RapidAPI-Key': '261a307990mshfd0d76825788385p17ba7fjsncded55b9bdf4',
-        'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
-    }
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '261a307990mshfd0d76825788385p17ba7fjsncded55b9bdf4',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
 };
 
 export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo/cities';
+
+
 
 export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
 export const WEATHER_API_KEY = "73f7c7ebf20035f6e974eff591fd7f47";
